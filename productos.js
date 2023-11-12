caa6edcfe03061ef42a715c20a41ae9c66f9e2d1
josuecalistro <calistrojosue03@gmail.com>
2023-11-12 00:20:56 -0300

    primer commit

---
+++ productos.js
@@ -0,0 +1,51 @@
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Estudiantes',
+        precio: 2800,
+        imagen: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwb4d45365/products/UA_1347975-600/UA_1347975-600-1.JPG'
+    },
+    {
+        id: 2,
+        nombre: 'River',
+        precio: 3200,
+        imagen: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw1eaa5dfb/products/ADHY3209/ADHY3209-1.JPG'
+    },
+    {
+        id: 3,
+        nombre: 'Racing',
+        precio: 3000,
+        imagen: 'https://kappaarg.vtexassets.com/arquivos/ids/233093-1200-auto?v=638318989837700000&width=1200&height=auto&aspect=true'
+    },
+    {
+        id: 4,
+        nombre: 'Independiente',
+        precio: 3100,
+        imagen: 'https://sporting.vtexassets.com/arquivos/ids/830840-1200-1200?width=1200&height=1200&aspect=true'
+    },
+    {
+        id: 5,
+        nombre: 'Boca',
+        precio: 3200,
+        imagen: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw975c9ca8/products/ADHY0280/ADHY0280-1.JPG'
+    },
+    {
+        id: 6,
+        nombre: 'San Lorenzo',
+        precio: 3000,
+        imagen: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw217677f4/products/NICK9892-490/NICK9892-490-1.JPG'
+    },
+    {
+        id: 7,
+        nombre: 'Huracan',
+        precio: 2800,
+        imagen: 'https://www.stockcenter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw32855903/products/KA2361I2YWA00B/KA2361I2YWA00B-1.JPG'
+    },
+    {
+        id: 8,
+        nombre: 'Gimnasia',
+        precio: 2900,
+        imagen: 'https://media.solodeportes.com.ar/media/catalog/product/cache/3cb7d75bc2a65211451e92c5381048e9/c/a/camiseta-de-gimnasia-givova-oficial-blanca-535023610103041-1.jpg'
+    }
+
+];
