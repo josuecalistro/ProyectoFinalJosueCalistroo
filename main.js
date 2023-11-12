73b09598e75347f7c9bb4f1fd51beefa111708a0
Calistro Josue <124108673+josuecalistro@users.noreply.github.com>
2023-11-12 14:56:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    const { value: email } = await Swal.fire({
+        title: "Ingresa tu correo electronico",
+        input: "email",
+        inputPlaceholder: "ejemplo@gmail.com"
+      });
+      if (email) {
+        Swal.fire(`Entered email: ${email}`);
+      }
+      const { value: password } = await Swal.fire({
+        title: "Ingresa tu Contraseña",
+        input: "password",
+        inputLabel: "Recuerde lo que ingrese",
+        inputPlaceholder: "contraseña",
+        inputAttributes: {
+          maxlength: "10",
+          autocapitalize: "off",
+          autocorrect: "off"
+        }
+      });
+      
+      
 
     let carrito = [];
     const divisa = '$';
@@ -9,40 +30,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const DOMbotonComprar = document.querySelector('#boton-comprar');
     const miLocalStorage = window.localStorage;
 
-    // Funciones
+    
 
-
-
-    /**
-    * Dibuja todos los productos a partir de la base de datos. No confundir con el carrito
-    */
     function renderizarProductos() {
         baseDeDatos.forEach((info) => {
-            // Estructura
+            
             const miNodo = document.createElement('div');
             miNodo.classList.add('card', 'col-sm-4');
-            // Body
+            
             const miNodoCardBody = document.createElement('div');
             miNodoCardBody.classList.add('card-body');
-            // Titulo
+            
             const miNodoTitle = document.createElement('h5');
             miNodoTitle.classList.add('card-title');
             miNodoTitle.textContent = info.nombre;
-            // Imagen
+            
             const miNodoImagen = document.createElement('img');
             miNodoImagen.classList.add('img-fluid');
             miNodoImagen.setAttribute('src', info.imagen);
-            // Precio
+            
             const miNodoPrecio = document.createElement('p');
             miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.textContent = `${info.precio}${divisa}`;
             miNodoPrecio.textContent = `${divisa}${info.precio}`;
-            // Boton
+    
             const miNodoBoton = document.createElement('button');
             miNodoBoton.classList.add('btn', 'btn-primary');
             miNodoBoton.textContent = '+';
             miNodoBoton.setAttribute('marcador', info.id);
             miNodoBoton.addEventListener('click', añadirProductoAlCarrito);
-            // Insertamos
+            
             miNodoCardBody.appendChild(miNodoImagen);
             miNodoCardBody.appendChild(miNodoTitle);
             miNodoCardBody.appendChild(miNodoPrecio);
@@ -51,133 +68,186 @@ document.addEventListener('DOMContentLoaded', () => {
             DOMitems.appendChild(miNodo);
         });
     }
-
-    /**
-    * Evento para añadir un producto al carrito de la compra
-    */
+    
+   function animacionAñadirAlCarrito(){
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Producto agregado al carrito de forma correcta"
+      });
+   }
     function añadirProductoAlCarrito(evento) {
-        // Anyadimos el Nodo a nuestro carrito
         carrito.push(evento.target.getAttribute('marcador'))
-        // Actualizamos el carrito
-        renderizarCarrito();
-        // Actualizamos el LocalStorage
+
+        animacionAñadirAlCarrito();
+        renderizarCarrito();      
         guardarCarritoEnLocalStorage();
     }
 
-    /**
-    * Dibuja todos los productos guardados en el carrito
-    */
+    function animacionElimiarProductoDelCarrito(evento){
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Producto eliminado del carrito de forma correcta"
+          });
+
+
+    }
+
     function renderizarCarrito() {
-        // Vaciamos todo el html
         DOMcarrito.textContent = '';
-        // Quitamos los duplicados
+        
         const carritoSinDuplicados = [...new Set(carrito)];
-        // Generamos los Nodos a partir de carrito
+        
         carritoSinDuplicados.forEach((item) => {
-            // Obtenemos el item que necesitamos de la variable base de datos
-            const miItem = baseDeDatos.filter((itemBaseDatos) => {
-                // ¿Coincide las id? Solo puede existir un caso
+            const miItem = baseDeDatos.filter((itemBaseDatos) => { 
                 return itemBaseDatos.id === parseInt(item);
             });
-            // Cuenta el número de veces que se repite el producto
             const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+               
                 return itemId === item ? total += 1 : total;
             }, 0);
-            // Creamos el nodo del item del carrito
+            
             const miNodo = document.createElement('li');
             miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
             miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
-            // Boton de borrar
+           
             const miBotonVaciar = document.createElement('button');
             miBotonVaciar.classList.add('btn', 'btn-danger', 'mx-5');
             miBotonVaciar.textContent = 'X';
             miBotonVaciar.style.marginLeft = '1rem';
             miBotonVaciar.dataset.item = item;
             miBotonVaciar.addEventListener('click', borrarItemCarrito);
-            // Mezclamos nodos
+            
             miNodo.appendChild(miBotonVaciar);
             DOMcarrito.appendChild(miNodo);
         });
-        // Renderizamos el precio total en el HTML
+       
         DOMtotal.textContent = calcularTotal();
     }
    
-    /**
-    * Evento para borrar un elemento del carrito
-    */
-    function borrarItemCarrito(evento) {
-        // Obtenemos el producto ID que hay en el boton pulsado
+    
+    function borrarItemCarrito(evento){
+      
         const id = evento.target.dataset.item;
-        // Borramos todos los productos
-        carrito = carrito.filter((carritoId) => {
-            return carritoId !== id;
-        });
-        // volvemos a renderizar
+      
+        const index = carrito.findIndex((carritoId) => carritoId === id);
+        if(index !== -1){
+            carrito.splice(index, 1);
+        };
+        animacionElimiarProductoDelCarrito();
+       
         renderizarCarrito();
-        // Actualizamos el LocalStorage
+        
         guardarCarritoEnLocalStorage();
-
     }
-
-    /**
-     * Calcula el precio total teniendo en cuenta los productos repetidos
-     */
+    
     function calcularTotal() {
-        // Recorremos el array del carrito
+        
         return carrito.reduce((total, item) => {
-            // De cada elemento obtenemos su precio
+            
             const miItem = baseDeDatos.filter((itemBaseDatos) => {
                 return itemBaseDatos.id === parseInt(item);
             });
-            // Los sumamos al total
+           
             return total + miItem[0].precio;
         }, 0).toFixed(2);
     }
-
-    /**
-    * Varia el carrito y vuelve a dibujarlo
-    */
+ 
+    function animacionVaciar() {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+    
+        return swalWithBootstrapButtons.fire({
+            title: "¿Estás seguro de vaciar el carrito?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Sí, vaciar",
+            cancelButtonText: "Cancelar",
+            
+           
+    reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                
+                carrito = [];
+                
+                renderizarCarrito();
+                
+                localStorage.clear();
+            }
+            
+            })
+    return result; 
+        };
+    
+    
     function vaciarCarrito() {
-        // Limpiamos los productos guardados
-        carrito = [];
-        // Renderizamos los cambios
-        renderizarCarrito();
-        // Borra LocalStorage
-        localStorage.clear();
-
+        
+        
+        animacionVaciar
+    animacionVaciar().then((result) => {
+            
+           
+    if (result.isConfirmed) {
+               
+                carrito = [];
+                
+    
+                renderizarCarrito();
+                
+                
+               
+    localStorage.clear();
+            }
+        });
     }
-
     function guardarCarritoEnLocalStorage () {
         miLocalStorage.setItem('carrito', JSON.stringify(carrito));
     }
-
     function cargarCarritoDeLocalStorage () {
-        // ¿Existe un carrito previo guardado en LocalStorage?
+       
         if (miLocalStorage.getItem('carrito') !== null) {
-            // Carga la información
+           
             carrito = JSON.parse(miLocalStorage.getItem('carrito'));
         }
     }
-
     
     function comprarCarrito() {
-        // Realiza la animación de compra (puedes agregar una función para animar aquí)
+        
     animacionCompra();
     
-        // Aquí puedes agregar la lógica para procesar la compra, enviar una solicitud al servidor, etc.
-    
-        // Después de la compra, puedes vaciar el carrito si es necesario
     vaciarCarrito();
     
     }
     
     function animacionCompra() {
         
-       
-    // Agrega aquí tu lógica de animación, por ejemplo, cambiar colores, agregar clases, etc.
-        // Puedes usar bibliotecas como jQuery o CSS para realizar animaciones.
-        // Aquí hay un ejemplo simple:
         const swalWithBootstrapButtons = Swal.mixin({
             customClass: {
               confirmButton: "btn btn-success",
@@ -200,7 +270,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 icon: "success"
               });
             } else if (
-              /* Read more about handling dismissals below */
+              
               result.dismiss === Swal.DismissReason.cancel
             ) {
               swalWithBootstrapButtons.fire({
@@ -211,12 +281,10 @@ document.addEventListener('DOMContentLoaded', () => {
             }
           });
     }
-
-    // Eventos
+    
     DOMbotonVaciar.addEventListener('click', vaciarCarrito);
     DOMbotonComprar.addEventListener('click', comprarCarrito);
 
-    // Inicio
     cargarCarritoDeLocalStorage();
     renderizarProductos();
     renderizarCarrito();
